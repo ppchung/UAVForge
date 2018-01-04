dd0b4bd1c36cdd2773b26a312ff4bc6f7487bd36
patrick <cyberp200@rocketmail.com>
2018-01-03 16:05:55 -0800

    testing filename modification on ostest.py

--- Avionics1718/imagecapture/ostest.py
+++ Avionics1718/imagecapture/ostest.py
@@ -3,3 +3,6 @@ from time import sleep
 os.system("sudo gphoto2 --capture-image-and-download --filename=000.jpg --force-overwrite")
 sleep(1)
 os.system("sudo gphoto2 --capture-image-and-download --filename=001.jpg --force-overwrite")
+photoNum = 3
+sleep(1)
+os.system("sudo gphoto2 --capture-image-and-download --filename=" + str(photoNum) + ".jpg --force-overwrite")
