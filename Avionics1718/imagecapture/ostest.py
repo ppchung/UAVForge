0ee658ffa5af234a2f38862806a0e699db4f3155
patrick <cyberp200@rocketmail.com>
2018-01-05 17:28:18 -0800

    switched from os to subprocess, seems to work better. using both subprocess.Popen and .call

--- Avionics1718/imagecapture/ostest.py
+++ Avionics1718/imagecapture/ostest.py
@@ -1,10 +1,31 @@
-import os
+import os, subprocess, signal
 from time import sleep
-os.system("sudo gphoto2 --capture-image-and-download --filename=000.jpg --force-overwrite")
+"""
+p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+out, err = p.communicate()
+for line in out.splitlines():
+	if b'gvfsd-gphoto2' in line:
+		pid = int(line.split(None, 1)[0])
+		os.kill(pid, signal.SIGKILL)
+"""
+p = subprocess.Popen(['sudo', 'gphoto2', '--capture-image-and-download', '--filename=000.jpg', '--force-overwrite'], stdout=subprocess.PIPE)
+out, err = p.communicate()
+print(out)
+#os.system("gphoto2 --camera "Sony Alpha-A6000 (Control)" --capture-image-and-download --filename=000.jpg --force-overwrite")
 sleep(1)
+p = subprocess.Popen(['sudo', 'gphoto2', '--capture-image-and-download', '--filename=001.jpg', '--force-overwrite'], stdout=subprocess.PIPE)
+out, err = p.communicate()
+sleep(1)
+photoNum = 3
+p = subprocess.Popen(['sudo', 'gphoto2', '--capture-image-and-download', '--filename='+str(photoNum)+'.jpg', '--force-overwrite'], stdout=subprocess.PIPE)
+out, err = p.communicate()
+sleep(1)
+subprocess.call(['sudo', 'feh', str(photoNum)+'.jpg', '-.'])
+"""
 os.system("sudo gphoto2 --capture-image-and-download --filename=001.jpg --force-overwrite")
 photoNum = 3
 sleep(1)
-os.system("sudo gphoto2 --capture-image-and-download --filename=" + str(photoNum) + ".jpg --force-overwrite")
+os.system("sudo gphoto2 --camera "Sony Alpha-A6000 (Control)" --capture-image-and-download --filename=" + str(photoNum) + ".jpg --force-overwrite")
 sleep(1)
 os.system("sudo feh " + str(photoNum) + ".jpg -.")
+"""
