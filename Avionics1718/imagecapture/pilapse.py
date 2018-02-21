22f59d944e41c389c36f9a339a57f7acdd757eb0
Patrick Chung <ppchung@uci.edu>
2018-02-21 04:51:30 +0000

    Updated version of GPhotoWrapper and the new pilapse have been added

---
+++ Avionics1718/imagecapture/pilapse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import RPi.GPIO as pin
+import GPhotoWrapper
+from time import sleep
+
+# Sets the image filename counter to zero
+x = 0
+
+# GPIO
+pin.setmode(pin.BCM)     # set up BCM GPIO numbering
+pin.setup(23, pin.IN,pull_up_down=pin.PUD_UP)    # set GPIO23 as input (button) this pin has an internal pullup
+pin.setup(25, pin.OUT)	 # set GPIO25 as output (LED) if it is solid, then it is takingpictures, if it blinks, then it is not
+try:
+	pin.output(25,0)
+	outputdata = GPhotoWrapper.summary()
+	while True:
+		while pin.input(23):
+			pin.output(25,1)	# Hold LED on to indicate IMG CPT is on
+			x = x + 1
+			outputdata = GPhotoWrapper.captureImageAndDownload("%Y%m%d_%H_%M_%S_Flight"+str(x)) # the subprocess command to take the image
+		pin.output(25,1)	# \
+		sleep(0.1)		#  \__Blinking procedure
+		pin.output(25,0)	#  /
+		sleep(0.9)		# /
+finally:
+	pin.cleanup()
